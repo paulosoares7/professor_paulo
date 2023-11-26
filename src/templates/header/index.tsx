3954e4c7151ca935bb27ba9f9423f2b0220f0603
paulosoares7 <paulosoaresrodrigues@outlook.com>
2023-11-26 02:55:37 -0400

    adição do componente button no header

--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -1,3 +1,4 @@
+import Btn from "@/components/Button";
 import NavigateLinks from "@/components/Navigate";
 import Link from "next/link";
 
@@ -14,7 +15,7 @@ export default function Header() {
               <div className= "px-6 py-2 duration-200 hover:text-pri-450 "><NavigateLinks label='Serviços' href="/"></NavigateLinks></div>
               <div className= "px-6 py-2 duration-200 hover:text-pri-450 "><NavigateLinks label='Sobre' href="/"></NavigateLinks></div>
               <div className= "px-6 py-2 duration-200 hover:text-pri-450 "><NavigateLinks label='Planos' href="/"></NavigateLinks></div>
-              <button className= "px-6 py-2 rounded-2xl duration-200 bg-pri-500 hover:bg-pri-450 hover:text-sec-450"><NavigateLinks label='Contato' href='./'></NavigateLinks></button>
+              <Btn><NavigateLinks label='Contato' href='./'></NavigateLinks></Btn>
             </nav>
           </div>
         </header>
