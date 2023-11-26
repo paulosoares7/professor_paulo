84d0f4a8cf28142ab4af0ba3f4e8d34759a75850
paulosoares7 <paulosoaresrodrigues@outlook.com>
2023-11-26 03:01:14 -0400

    adicionar Template Header em Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "@/templates/header";
+
 export default function Home() {
   return (
     <div className=" h-screen bg-pri-950 flex flex-col">
-      
+      <Header/>
     </div>
   )
 }
