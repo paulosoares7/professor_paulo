3c1ca18d8a755b951669b312d2623a18bcc41633
paulosoares7 <paulosoaresrodrigues@outlook.com>
2023-11-26 02:35:17 -0400

    criação do compenente de navegação

---
+++ src/components/Navigate/index.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+interface NavigateLinksProps {
+    href: string,
+    label: string,
+}
+
+export default function NavigateLinks({href,label}: NavigateLinksProps) {
+    return (
+      <Link href={`/${href}`}>{label}</Link>
+    )
+  }
+  
