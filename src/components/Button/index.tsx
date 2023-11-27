97082e9aa1a2f4e1930e15517c31e54f27412acb
paulosoares7 <paulosoaresrodrigues@outlook.com>
2023-11-27 18:30:58 -0400

    Ajustanto padding e fonte

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export default function Btn({children}: ButtonProps) {
     return (
-      <button className= "px-6 py-2 rounded-2xl duration-200 bg-pri-500 hover:bg-pri-450 hover:text-sec-450">
+      <button className= "px-6 py-2 rounded-2xl duration-200 font-semibold bg-pri-500 hover:bg-pri-450 hover:text-sec-450">
         {children}
       </button>
     )
--- src/templates/header/index.tsx
+++ src/templates/header/index.tsx
@@ -1,16 +1,17 @@
 import Btn from "@/components/Button";
 import NavigateLinks from "@/components/Navigate";
-import Link from "next/link";
 
 export default function Header() {
     return (
-        <header className="text-pri-50 h-16 p-1 font-sans"> 
+        <header className="text-pri-50 h-16 py-1 px-6 font-sans"> 
+
           <div className= "h-full px-2 py-2 flex items-center justify-between">
             <div className=" flex flex-col items-start justify-center flex-1 px-6 h-full">
+              
               <h1 className="text-lg leading-none font-bold">Paulo Soares</h1>
-              <p className="text-sm leading-none font-extralight">Prof. de Física</p>
+              <p className="text-sm leading-none font-medium">Prof. de Física</p>
             </div>
-            <nav className="flex items-center justify-between gap-3 h-full ">
+            <nav className="flex items-center justify-between gap-3 h-full font-semibold">
               <div className= "px-6 py-2 duration-200 hover:text-pri-450"><NavigateLinks label = 'Início' href="/"></NavigateLinks></div>
               <div className= "px-6 py-2 duration-200 hover:text-pri-450 "><NavigateLinks label='Serviços' href="/"></NavigateLinks></div>
               <div className= "px-6 py-2 duration-200 hover:text-pri-450 "><NavigateLinks label='Sobre' href="/"></NavigateLinks></div>
