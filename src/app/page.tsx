ca28f98eee3d42a27750784edb78fa82988a02ea
paulosoares7 <paulosoaresrodrigues@outlook.com>
2023-11-27 18:49:11 -0400

    criação da primeira pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import GetStart from "@/templates/GetStart";
 import Header from "@/templates/header";
 
 export default function Home() {
   return (
     <div className=" h-screen bg-pri-950 flex flex-col">
       <Header/>
+      <GetStart/>
+      
     </div>
   )
 }
